dc15a96cdd7907df124eda27329898624da8ea27
Erick Cafferata <nagato@god.com>
2016-06-04 14:48:29 -0300

    Port to SimpleBoard(STM32F103C8)
    - RCC configuration function added.
    - Board-specific GPIO port.

--- src/main.c
+++ src/main.c
@@ -38,6 +38,48 @@ GPIO_InitTypeDef GPIO_InitStructure;
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
+//#define HSEON
+void RCC_Configuration(void)
+{
+  ErrorStatus HSEStartUpStatus;
+  /* RCC system reset(for debug purpose) */
+  RCC_DeInit();
+#ifdef HSEON
+  /* Enable HSE */
+  RCC_HSEConfig(RCC_HSE_ON);
+  ///* Wait till HSE is ready */
+  HSEStartUpStatus = RCC_WaitForHSEStartUp();
+  if(HSEStartUpStatus == SUCCESS)
+  {
+#endif
+    /* HCLK = SYSCLK */
+    RCC_HCLKConfig(RCC_SYSCLK_Div1);
+    /* PCLK2 = HCLK */
+    RCC_PCLK2Config(RCC_HCLK_Div1);
+    /* PCLK1 = HCLK/2 */
+    RCC_PCLK1Config(RCC_HCLK_Div2);
+    /* Flash 2 wait state */
+    FLASH_SetLatency(FLASH_Latency_2);
+    /* Enable Prefetch Buffer */
+    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+    /* PLLCLK = 8MHz * 9 = 72 MHz */
+    #ifdef HSEON
+    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_16);
+    #else
+    RCC_PLLConfig(RCC_PLLSource_HSI_Div2, RCC_PLLMul_16);
+    #endif
+    /* Enable PLL */
+    RCC_PLLCmd(ENABLE);
+    /* Wait till PLL is ready */
+    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+    /* Select PLL as system clock source */
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+    /* Wait till PLL is used as system clock source */
+    while(RCC_GetSYSCLKSource() != 0x08);
+#ifdef HSEON
+  }
+#endif
+}
 
 /**
   * @brief  Main program.
@@ -54,13 +96,14 @@ int main(void)
      */
 
   /* GPIOD Periph clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  RCC_Configuration();
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
   /* Configure PA0 and PA2 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
 
   /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory.
      You can monitor PA0 or PA2 on the scope to measure the output signal.
@@ -71,10 +114,10 @@ int main(void)
   {
     volatile int i;
     /* Set PA0 and PA2 */
-    GPIOA->BSRR = 0x00000003;
+    GPIOB->BSRR = 0x00000080;
     for(i=1000000;i>0;i--);
     /* Reset PA0 and PA2 */
-    GPIOA->BRR  = 0x00000003;
+    GPIOB->BRR  = 0x00000080;
     for(i=1000000;i>0;i--);
   }
 }
